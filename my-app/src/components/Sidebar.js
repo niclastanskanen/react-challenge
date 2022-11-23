207a4503917f3571ff1ec0ef34563b2150f88621
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-23 11:04:22 +0000

    add sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Sidebar = () => <div>
+    <a href="#">My Photos</a>
+    <a href="#">My Illustrations</a>
+    <a href="#">My Paintings</a>
+</div>;
+
+export default Sidebar;
