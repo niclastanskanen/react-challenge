c29ba95e541025eb21e49b5b5ad5c4058f0d75f0
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-23 11:15:43 +0000

    add style to sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,9 +1,11 @@
 import React from "react"
+import css from "./css/Sidebar.module.css"
 
-const Sidebar = () => <div>
-    <a href="#">My Photos</a>
-    <a href="#">My Illustrations</a>
-    <a href="#">My Paintings</a>
+const Sidebar = () => 
+<div className={css.sidebar}>
+    <a href="#" target="_blank">My Photos</a>
+    <a href="#" target="_blank">My Illustrations</a>
+    <a href="#" target="_blank">My Paintings</a>
 </div>;
 
 export default Sidebar;
