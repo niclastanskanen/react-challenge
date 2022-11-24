aa32e3ea0565b2d8c57f670e7ca3db61c042c83d
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-24 08:42:12 +0000

    add navbar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -3,9 +3,9 @@ import css from "./css/Sidebar.module.css"
 
 const Sidebar = () => 
 <div className={css.sidebar}>
-    <a href="#" target="_blank">My Photos</a>
-    <a href="#" target="_blank">My Illustrations</a>
-    <a href="#" target="_blank">My Paintings</a>
+    <a href="#." target="_blank">My Photos</a>
+    <a href="#." target="_blank">My Illustrations</a>
+    <a href="#." target="_blank">My Paintings</a>
 </div>;
 
 export default Sidebar;
